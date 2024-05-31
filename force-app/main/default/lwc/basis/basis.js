f9a7ee5057758baae569ab713e64aa7b721c97af
Zoltan Rabi <zoltan.rabi@accenture.com>
2024-05-31 11:31:29 +0200

    delete old examples

--- force-app/main/default/lwc/basis/basis.js
+++
@@ -1,9 +0,0 @@
-import { api, LightningElement, track, wire } from 'lwc';
-
-export default class Basis extends LightningElement {
-    certTypeName;
-
-    handleCertSelected(evt) {
-        this.certTypeName = evt.detail;
-    }
-}
